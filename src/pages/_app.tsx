3d7a001352a9975776f29654e506ea482f1c1887
I wanna be real nerd <71207364+IwannabeRealnerD@users.noreply.github.com>
2024-06-29 22:03:42 +0900

    lint-staged implemented

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,5 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const test: any = 3;
-  const test1g: any = 3;
   return <Component {...pageProps} />;
 }
