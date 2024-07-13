e36fc9068eafaf206850b1853cff530e2786b61a
I wanna be real nerd <71207364+IwannabeRealnerD@users.noreply.github.com>
2024-07-13 12:30:40 +0900

    [feat] next-bundle-analyzer (#38)
    * [feat] add next-buldle-analyzer
    
    * [fix] fix lint error

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,6 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
-export default function Document() {
+export default () => {
   return (
     <Html lang="en">
       <Head />
@@ -10,4 +10,4 @@ export default function Document() {
       </body>
     </Html>
   );
-}
+};
