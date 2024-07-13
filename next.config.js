282f93a04aa9ee93ca4c5de4bea181bb2558c550
I wanna be real nerd <71207364+IwannabeRealnerD@users.noreply.github.com>
2024-07-13 12:25:27 +0900

    [feat] add next-buldle-analyzer

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
+import bundleAnalyzer from "@next/bundle-analyzer";
 import { createVanillaExtractPlugin } from "@vanilla-extract/next-plugin";
+
+const withBundleAnalyzer = bundleAnalyzer({ enabled: process.env.ANALYZE === "true" });
 
 const withVanillaExtract = createVanillaExtractPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = { reactStrictMode: true };
 
-export default withVanillaExtract(nextConfig);
+export default withBundleAnalyzer(withVanillaExtract(nextConfig));
