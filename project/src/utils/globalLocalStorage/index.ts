1a6b9b52125c9bf55836f4c0d5d2449923db1603
I wanna be real nerd <71207364+IwannabeRealnerD@users.noreply.github.com>
2024-09-23 19:15:12 +0900

    fix: fix storage apporace function

--- project/src/utils/globalLocalStorage/index.ts
+++ project/src/utils/globalLocalStorage/index.ts
@@ -16,21 +16,24 @@ export const globalGetLocalStorage = <T extends keyof LocalStorage>(key: T) => {
 };
 
 export const globalSetLocalStorage = <K extends keyof LocalStorage>(key: K, value: LocalStorage[K]) => {
-  const serializeDate = (deSerializedValue: LocalStorage[K]): unknown => {
+  const serializeData = (deSerializedValue: unknown): unknown => {
     if (deSerializedValue instanceof Date) {
       return `${GLOBAL_DATE_PREFIX}${deSerializedValue.toISOString()}`;
     }
     if (deSerializedValue === null) {
       return null;
     }
+    if (Array.isArray(deSerializedValue)) {
+      return deSerializedValue.map((item) => serializeData(item));
+    }
     if (typeof deSerializedValue === "object") {
       return Object.fromEntries(
-        Object.entries(deSerializedValue).map(([_key, _value]) => [_key, serializeDate(_value)])
+        Object.entries(deSerializedValue).map(([_key, _value]) => [_key, serializeData(_value)])
       );
     }
     return deSerializedValue;
   };
-  localStorage.setItem(key, JSON.stringify(serializeDate(value)));
+  localStorage.setItem(key, JSON.stringify(serializeData(value)));
 };
 
 export const globalRemoveLocalStorage = (key: keyof LocalStorage) => {
--- project/src/utils/globalLocalStorage/type.ts
+++ project/src/utils/globalLocalStorage/type.ts
@@ -1,21 +1,4 @@
 export type LocalStorage = {
   accessToken: string;
   refreshToken: string;
-  userUUID: string;
-  example: {
-    foo: string;
-    bar: number;
-  };
-  nestedExample: {
-    lv1: {
-      lv2: {
-        date: Date;
-        string: string;
-        number: number;
-        null: null;
-        undefined: undefined;
-      };
-    };
-  };
-  testDate: Date;
 };
