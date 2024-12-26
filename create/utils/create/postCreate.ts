078cb851fe5626dfa5a88ed17ad103246ab6382c
I wanna be real nerd <71207364+IwannabeRealnerD@users.noreply.github.com>
2024-12-26 22:51:17 +0900

    [FEAT] add git init (#81)
    * misc: delete unnecessary return
    
    * feat: add git init in create process
    
    * feat: change the location of git init logic

--- create/utils/create/postCreate.ts
+++ create/utils/create/postCreate.ts
@@ -1,7 +1,20 @@
+import { execSync } from "child_process";
+
 import { logWithColor } from "../logWithColor";
 
 export const postCreate = (projectLocation: string) => {
   logWithColor(`\nProject is created successfully at ${projectLocation}\n`, "blue");
+  try {
+    execSync("git init", { cwd: projectLocation });
+    execSync("git add .", { cwd: projectLocation });
+    execSync('git commit -m "Initial commit"', {
+      cwd: projectLocation,
+    });
+    logWithColor(`Git initialized successfully.\n`, "green");
+  } catch (error) {
+    logWithColor(`Error occurred while initializing git: ${error}`, "red");
+    logWithColor(`Please initialize git manually.\n`, "red");
+  }
   logWithColor(`You can start development by running the following command.\n`, "blue");
   logWithColor(`  cd ${projectLocation}`, "green");
   logWithColor(`  pnpm i`, "green");
