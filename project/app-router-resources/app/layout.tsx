16775cea398a9786c5c2c77dd3406cec264de60b
I wanna be real nerd <71207364+IwannabeRealnerD@users.noreply.github.com>
2025-02-13 19:39:43 +0900

    feat: add settings for tailwindcss

--- project/app-router-resources/app/layout.tsx
+++ project/app-router-resources/app/layout.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 
+import "@/styles/app.css";
+
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
