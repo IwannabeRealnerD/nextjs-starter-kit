3b596054f4ff057ca85633a36bdea7cb9ee4abd4
I wanna be real nerd <71207364+IwannabeRealnerD@users.noreply.github.com>
2025-02-14 00:45:54 +0900

    feat: add cn with tailwind-merge

--- project/src/pages/index.page.tsx
+++ project/src/pages/index.page.tsx
@@ -1,5 +1,5 @@
 const IndexPage = () => {
-  return <h2 className="bg-pink-500">Created by create-nextjs-starter-kit</h2>;
+  return <h2 className="bg-pink-500 text-2xl">Created by create-nextjs-starter-kit</h2>;
 };
 
 export default IndexPage;
