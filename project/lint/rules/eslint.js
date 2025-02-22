9c15d5bf4591284b725f9fdbba2d42f3535ec544
I wanna be real nerd <71207364+IwannabeRealnerD@users.noreply.github.com>
2025-02-22 22:53:03 +0900

    [feat] Remove Pages Router Support (#86)
    * feat: delete pages router codes in project-app
    
    * feat: delete pages router codes in project-app
    
    * fix: fix build command for existing file on create-app
    
    * fix: remove not exising fonts
    
    * chore: pnpm version sync

--- project/lint/rules/eslint.js
+++ project/lint/rules/eslint.js
@@ -1,7 +1,7 @@
 import js from "@eslint/js";
 
 export const eslintRules = {
+  ...js.configs.recommended.rules,
   "no-unused-vars": "off",
   "arrow-body-style": "off",
-  ...js.configs.recommended.rules,
 };
--- project/src/pages/index.page.tsx
+++
@@ -1,5 +0,0 @@
-const IndexPage = () => {
-  return <h2 className="bg-pink-500 text-2xl">Created by create-nextjs-starter-kit</h2>;
-};
-
-export default IndexPage;
