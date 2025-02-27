85bfc0d169093a8c36ada595c6f5606b6b6d3546
I wanna be real nerd <71207364+IwannabeRealnerD@users.noreply.github.com>
2025-02-27 20:24:13 +0900

    feat: add object-shorthand rules

--- project/lint/rules/eslint.js
+++ project/lint/rules/eslint.js
@@ -2,6 +2,7 @@ import js from "@eslint/js";
 
 export const eslintRules = {
   ...js.configs.recommended.rules,
+  "object-shorthand": ["error", "always"],
   "no-unused-vars": "off",
   "arrow-body-style": "off",
 };
