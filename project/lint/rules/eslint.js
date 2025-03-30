a87bc53335323baf07b269819b6fb9de99004837
I wanna be real nerd <71207364+IwannabeRealnerD@users.noreply.github.com>
2025-03-30 10:11:48 +0900

    chore: activate curly rule in ESLint

--- project/lint/rules/eslint.js
+++ project/lint/rules/eslint.js
@@ -4,5 +4,6 @@ export const eslintRules = {
   ...js.configs.recommended.rules,
   "object-shorthand": ["error", "always"],
   "no-unused-vars": "off",
+  curly: "error",
   "arrow-body-style": "off",
 };
