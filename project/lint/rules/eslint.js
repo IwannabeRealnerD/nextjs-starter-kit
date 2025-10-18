d27d56baf26ddf8865e4d921cedefecad5acf5db
IwannabeRealnerD <IwannabeRealnerD@gmail.com>
2025-10-18 11:03:25 +0900

    feat: add lint rule for Boolean casting

--- project/lint/rules/eslint.js
+++ project/lint/rules/eslint.js
@@ -7,4 +7,11 @@ export const eslintRules = {
   "arrow-body-style": "off",
   "no-redeclare": "off",
   "no-console": "warn",
+  "no-restricted-syntax": [
+    "error",
+    {
+      selector: "UnaryExpression[operator='!'][argument.type='UnaryExpression'][argument.operator='!']",
+      message: "Use 'Boolean(value)' instead of '!!value' for explicit boolean casting.",
+    },
+  ],
 };
